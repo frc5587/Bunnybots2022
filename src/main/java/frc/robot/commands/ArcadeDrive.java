154bf8676eff53dc560e3de56415b292d930eca8
Fafa0079 <rward0079@outlook.com>
2024-06-07 16:22:34 -0500

    updated libraries and lowered speed

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -8,8 +8,8 @@ import frc.robot.subsystems.Drivetrain;
 
 public class ArcadeDrive extends ArcadeDriveBase {
     public ArcadeDrive(Drivetrain drivetrain, DoubleSupplier throttleSupplier, DoubleSupplier curveSupplier) {
-        //super(drivetrain, () -> MathUtil.clamp(throttleSupplier.getAsDouble(), -0.4, 0.4), () -> MathUtil.clamp(curveSupplier.getAsDouble(), -0.4, 0.4));
-        super(drivetrain, throttleSupplier, curveSupplier);
+        super(drivetrain, () -> MathUtil.clamp(throttleSupplier.getAsDouble(), -0.4, 0.4), () -> MathUtil.clamp(curveSupplier.getAsDouble(), -0.4, 0.4));
+        // super(drivetrain, throttleSupplier, curveSupplier);
     }
     
 }
