76bac86080dab2a04953f12745ec0e5b119bd650
Ricky Ward <rward0079@outlook.com>
2025-02-01 15:05:12 -0500

    attempt to fix code

--- src/main/java/frc/robot/config/REVConfigs.java
+++ src/main/java/frc/robot/config/REVConfigs.java
@@ -1,21 +1,21 @@
 package frc.robot.config;
 
-import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import frc.robot.Constants.DrivetrainConstants;
 
 public class REVConfigs {
-    public SparkBaseConfig leftConfig;
-    public SparkBaseConfig rightConfig;
+    public SparkMaxConfig leftConfig = new SparkMaxConfig();
+    public SparkMaxConfig rightConfig = new SparkMaxConfig();
 
     public REVConfigs() {
-        leftConfig.smartCurrentLimit(DrivetrainConstants.STALL_CURRENT_LIMIT, DrivetrainConstants.FREE_CURRENT_LIMIT);
-        leftConfig.idleMode(IdleMode.kBrake);
-        leftConfig.inverted(DrivetrainConstants.LEFT_MOTOR_INVERTED);
+        leftConfig.smartCurrentLimit(DrivetrainConstants.STALL_CURRENT_LIMIT, DrivetrainConstants.FREE_CURRENT_LIMIT)
+            .idleMode(IdleMode.kBrake)
+            .inverted(DrivetrainConstants.LEFT_MOTOR_INVERTED);
 
-        rightConfig.apply(leftConfig);
-        rightConfig.inverted(DrivetrainConstants.RIGHT_MOTOR_INVERTED);
+        rightConfig.apply(leftConfig)
+            .inverted(DrivetrainConstants.RIGHT_MOTOR_INVERTED);
 
     }
 }
